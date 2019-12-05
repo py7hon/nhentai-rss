3d46087aae77e571a2eac39b0beabfa10b0e7e56
iqbal rifai <iqbalrifai@waifu.club>
2019-12-05 18:10:26 +0700

    Update index.php
    Make it Better

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 //Convert JSONfeed to RSS in a single function as a drop-in to make adding JSONfeed
 //support to an aggregator easier
 function convert_jsonfeed_to_rss($content = NULL, $max = NULL)
@@ -24,25 +23,23 @@ function convert_jsonfeed_to_rss($content = NULL, $max = NULL)
     //$lastBuildDate = date(DATE_RSS, $latestDate);
 
     //Create the RSS feed
-    $xmlFeed = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"
-	xmlns:content="http://purl.org/rss/1.0/modules/content/"
-	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
-	xmlns:dc="http://purl.org/dc/elements/1.1/"
-	xmlns:atom="http://www.w3.org/2005/Atom"
-	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
-	xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
-	xmlns:image="http://purl.org/rss/1.0/modules/image/"
-	></rss>');
+    $xmlFeed = new SimpleXMLElement('<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" version="2.0"></rss>');
     $xmlFeed->addChild("channel");
 
     //Required elements
-    $nhentai = 'https://nhentai/g/'.$item['id'];
+    $nhentai = 'https://nhentai.net/g/'.$item['id'];
     $web = 'https://nhentai.net';
     //$data = '<img src="https://t.nhentai.net/galleries/'.$item['media_id'].'/cover.jpg"/>';
-    $xmlFeed->channel->addChild("title", 'nHentai RSS Feed by. Iqbal Rifai');
-    $xmlFeed->channel->addChild("pubDate", $tanggal1);
-    $xmlFeed->channel->addChild("lastBuildDate", $tanggal1);
+    $xmlFeed->channel->addChild("title", "<![CDATA[ nHentai - lastest ]]>");
     $xmlFeed->channel->addChild("link", $web);
+    //$xmlFeed->channel->addChild("atom:link", 'href="http://rsshub.app/nhentai/language/english" rel="self" type="application/rss+xml"');
+    $xmlFeed->channel->addChild("description", "<![CDATA[ nhentai - Made with love by Iqbal Rifai(https://github.com/py7hon/nhentai-rss ) ]]>");
+    $xmlFeed->channel->addChild("generator", "nHentai RSS");
+    $xmlFeed->channel->addChild("webMaster", "nhentai@rape.lol (SnowFagz)");
+    $xmlFeed->channel->addChild("language", "en-us");
+    $xmlFeed->channel->addChild("pubDate", $tanggal1);
+    $xmlFeed->channel->addChild("ttl", "50");
+    //$xmlFeed->channel->addChild("lastBuildDate", $tanggal1);
     //$xmlFeed->channel->createElementNS("media:thumbnail", $gambar);
     //$xmlFeed->channel->addChild("link", $gambar);
     //$enclosure = $newItem->addChild('enclosure');
@@ -53,18 +50,21 @@ function convert_jsonfeed_to_rss($content = NULL, $max = NULL)
     //Items
     foreach ($jf['result'] as $item) {
         $newItem = $xmlFeed->channel->addChild('item');
+        $node = $newItem->addChild( 'image', Null, 'http://search.yahoo.com/mrss/' );
         $a[1] = ($item["images"]["cover"]["t"] == "p") ? "png" : "jpg";
         //$tags = $item["tags"]["name"];
         $gambar = 'https://t.nhentai.net/galleries/'.$item['media_id'].'/cover.'.$a[1];
+        $srcg = '<![CDATA[ <img src="'.$gambar.'"/> ]]>';
         $tanggal = gmdate('Y-m-d H:i:s', $item['upload_date']);
         //Standard stuff
         if (isset($item['id'])) $newItem->addChild('guid', $item['id']);
-        if (isset($item['title']['pretty'])) $newItem->addChild('title', $item['title']['pretty']);
+        if (isset($item['title']['english'])) $newItem->addChild('title', $item['title']['english']);
         if (isset($item['title']['english'])) $newItem->addChild('description', $item['title']['english']);
         //if (isset($gambar)) $newItem->createElementNS("media:thumbnail", $gambar);
-        if (isset($gambar)) $newItem->addChild('image', $gambar);
+        //if (isset($gambar)) $newItem->addChild('image', $gambar);
         if (isset($tanggal)) $newItem->addChild('pubDate', $tanggal);
         if (isset($nhentai)) $newItem->addChild('link',  $nhentai);
+        if (isset($gambar)) $node->addAttribute('url', $gambar);
     }
 
     //Make the output pretty
